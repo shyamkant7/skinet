373797cf90d5d3b6f65bbbc3885ee434d2eb9ac9
Shyam <shyam.deshmukh07@gmail.com>
2020-04-15 01:38:48 -0400

    End of Section 10

--- client/src/app/shop/shop.module.ts
+++ client/src/app/shop/shop.module.ts
@@ -1,22 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
 import { ShopComponent } from './shop.component';
 import { ProductItemComponent } from './product-item/product-item.component';
 import { SharedModule } from '../shared/shared.module';
-
+import { ProductDetailsComponent } from './product-details/product-details.component';
+import { ShopRoutingModule } from './shop-routing.module';
 
 
 @NgModule({
   declarations: [
     ShopComponent,
-    ProductItemComponent
+    ProductItemComponent,
+    ProductDetailsComponent
   ],
   imports: [
     CommonModule,
-    SharedModule
-  ],
-  exports: [
-    ShopComponent
+    RouterModule,
+    SharedModule,
+    ShopRoutingModule
   ]
 })
 export class ShopModule { }
